834811f8395ded7ff7057b61814a924a4a41bc66
Dejan <dejanhadziomerovic@gmail.com>
2018-04-29 15:07:35 +0200

    style single, about us, footer

--- single.php
+++ single.php
@@ -9,22 +9,68 @@
 <!-- Page title section ENDS -->
 
 <!-- Content section STARTS -->
-<div class="container">
-    <section class="section section--regular">
-        <div class="post-excerpt">
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+<div class="container pb-50">
+    <div class="row">
+        <div class="col-9">
+            <section class="section section--single">
+                <div class="post-excerpt">
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+                <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</p>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+                <img src="assets/img/fedex-truck.jpg" alt="">
+                <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</p>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+                <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</p>
+
+                <a href="posts.php" class="button button--inverse mt-20">Back to all posts</a>
+
+            </section>
         </div>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
-        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</p>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
-        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</p>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
-        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</p>
-
-        <a href="posts.php" class="button button--inverse">Back to all posts</a>
-
-    </section>
-
+        <div class="col-3">
+            <div class="main-sidebar">
+                <div class="info-block info-block--news">
+                    <h3>Recent News</h3>
+                    <ul class="news-list list-unstyled">
+                        <li class="mb-3">
+                            <article class="news-list__article--sidebar">
+                                <a href="#" class="d-flex flex-start">
+                                    <img src="http://lorempixel.com/110/90" alt="">
+                                    <div class="news-list__article-content">
+                                        <h4>FedEx and UPS: Competing CEOs Agree on Reforms</h4>
+                                        <span class="date">11.11.2017.</span>
+                                    </div>
+                                </a>
+                            </article>
+                        </li>
+                        <li class="mb-3">
+                            <article class="news-list__article--sidebar">
+                                <a href="#" class="d-flex flex-start">
+                                    <img src="http://lorempixel.com/110/90" alt="">
+                                    <div class="news-list__article-content">
+                                        <h4>From Engines to Tail Lights, FedEx Technicians’ Knowledge On Display</h4>
+                                        <span class="date">11.11.2017.</span>
+                                    </div>
+                                </a>
+                            </article>
+                        </li>
+                        <li class="mb-3">
+                            <article class="news-list__article--sidebar">
+                                <a href="#" class="d-flex flex-start">
+                                    <img src="http://lorempixel.com/110/90" alt="">
+                                    <div class="news-list__article-content">
+                                        <h4>FedEx and UPS: Competing CEOs Agree on Reforms</h4>
+                                        <span class="date">11.11.2017.</span>
+                                    </div>
+                                </a>
+                            </article>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- Content section ENDS -->
 
--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,22 @@
     <footer class="main-footer">
         <div class="container">
             <div class="row">
-				<div class="col-4">
+				<div class="col-3">
 					<div class="info-block info-block--contact">
                         <h4>Flying Cargo YU doo</h4>
+                        <div class="social-block">
+                            <ul class="social-block__list list-unstyled">
+                                <li>
+                                    <a href="#">
+                                        <i class="mdi mdi-facebook-box"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+				</div>
+                    <div class="col-3">
+                        <h4>Filijala Beograd</h4>
                         <ul class="contact-list list-unstyled">
                             <li class="d-flex align-items-center">
                                 <i class="mdi mdi-map-marker small-icon mr-2"></i>
@@ -12,10 +25,6 @@
                             <li class="d-flex align-items-center">
                                 <i class="mdi mdi-phone small-icon mr-2"></i>
                                 <span>+381 11 3109 400</span>
-                            </li>
-                            <li class="d-flex align-items-center">
-                                <i class="mdi mdi-fax small-icon mr-2"></i>
-                                <span>+381 11 3109 444</span>
                             </li>
                             <li class="d-flex align-items-center">
                                 <i class="mdi mdi-email small-icon mr-2"></i>
@@ -30,23 +39,66 @@
                             </li>
                         </ul>
                     </div>
-				</div>
-                <div class="col-4 social-block text-center">
-                    <!-- <h4>Follow us on social networks</h4> -->
-                    <ul class="social-block__list list-unstyled">
-                        <li>
-                            <a href="#">
-                                <i class="mdi mdi-facebook-box"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-4 text-right">
-                    <span>All rights reserved. © Flying Cargo YU doo 2000-2017</span>
+                    <div class="col-3">
+                        <h4>Carinsko odeljenje</h4>
+                        <ul class="contact-list list-unstyled">
+                            <li class="d-flex align-items-top">
+                                <i class="mdi mdi-map-marker small-icon mr-2"></i>
+                                <span>Aerodrom Beograd</br>
+                                    Robno carinski magacin</br>
+                                </span>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <i class="mdi mdi-phone small-icon mr-2"></i>
+                                <span>+381 11 22 86 135</span>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <i class="mdi mdi-email small-icon mr-2"></i>
+                                <span>customs@flying-cargo.rs</span>
+                            </li>
+                            <li class="d-flex align-items-top">
+                                <i class="mdi mdi-clock small-icon mr-2"></i>
+                                <span class="mt-1">Mon - Fri: 08:00 to 19:00</br>
+                                    Sat: 08:00 to 14:00</br>
+                                    Closed on Sundays and public holidays
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <h4>Filijala Novi Sad</h4>
+                        <ul class="contact-list list-unstyled">
+                            <li class="d-flex align-items-center">
+                                <i class="mdi mdi-map-marker small-icon mr-2"></i>
+                                <span>Šafarikova 35, 21000 Novi Sad</span>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <i class="mdi mdi-phone small-icon mr-2"></i>
+                                <span>+381 21 571 515</span>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <i class="mdi mdi-fax small-icon mr-2"></i>
+                                <span>+381 11 3109 444</span>
+                            </li>
+                            <li class="d-flex align-items-center">
+                                <i class="mdi mdi-email small-icon mr-2"></i>
+                                <span>info@flying-cargo.rs</span>
+                            </li>
+                            <li class="d-flex align-items-top">
+                                <i class="mdi mdi-clock small-icon mr-2"></i>
+                                <span class="mt-1">Mon - Fri: 08:00 to 19:00</br>
+                                    Sat: 08:00 to 14:00</br>
+                                    Closed on Sundays and public holidays
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-12 text-left pt-50 small">
+                        <span>All rights reserved. © Flying Cargo YU doo 2000-2018</span>
+                    </div>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
 
     </body>
 </html>
